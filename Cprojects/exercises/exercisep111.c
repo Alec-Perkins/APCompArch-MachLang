958bd42838c12d631d0721019a23fb913b123ce0
Alec R. Perkins <alecperkinsw@gmail.com>
2021-08-30 20:17:37 -0700

    completed exercise 3.35

--- Cprojects/exercises/exercisep111.c
+++ Cprojects/exercises/exercisep111.c
@@ -4,6 +4,19 @@
 #include <stdio.h>
 
 int main(){
+    unsigned int bin;
+    unsigned int decimal = 0;
+    unsigned int place = 1;
 
+    printf("Input a binary number (5 digits or fewer): ");
+    scanf("%u", &bin);
+
+    while (bin > 0){
+        decimal += ((bin % 10) * place);
+        bin /= 10;
+        place *= 2;
+    }
+
+    printf("decimal: %u\n", decimal);
     return 0;
 }
