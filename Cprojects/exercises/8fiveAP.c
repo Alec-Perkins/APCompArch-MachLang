ab431869be45e4feaaba5e27f621b24e5f27d951
Perkins <803370@suhsd-staff.seq.org>
2021-10-22 19:42:38 +0100

    Created 8fiveAP

---
+++ Cprojects/exercises/8fiveAP.c
@@ -0,0 +1,18 @@
+// Exercises 8.5 - 8.8
+// A. Perkins
+
+#include <stdio.h>
+#include <ctype.h>
+
+void charHandling(char c);
+
+int main()
+{
+    printf("im thinking about it right now");
+    return 0;
+}
+
+void charHandling(char c)
+{
+    
+}
