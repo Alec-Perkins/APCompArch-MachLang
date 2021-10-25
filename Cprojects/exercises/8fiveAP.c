0aa651ae6155abd25df375f951556fd457c68e45
Perkins <803370@suhsd-staff.seq.org>
2021-10-25 19:20:19 +0100

    Created charhandling in 8fiveAP.c

--- Cprojects/exercises/8fiveAP.c
+++ Cprojects/exercises/8fiveAP.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void charHandling(char c);
+void charHandling(int c);
 
 int main()
 {
@@ -12,7 +12,20 @@ int main()
     return 0;
 }
 
-void charHandling(char c)
+void charHandling(int c)
 {
-    
+    printf("%d", isblank(c));
+    printf("%d", isdigit(c));
+    printf("%d", isalpha(c));
+    printf("%d", isalnum(c));
+    printf("%d", isxdigit(c));
+    printf("%d", islower(c));
+    printf("%d", isupper(c));
+    printf("%c", tolower(c));
+    printf("%c", toupper(c));
+    printf("%d", isspace(c));
+    printf("%d", iscntrl(c));
+    printf("%d", ispunct(c));
+    printf("%d", isprint(c));
+    printf("%d", isgraph(c));
 }
