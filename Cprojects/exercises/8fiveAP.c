9bdc973629de24c3ff269ba7345a88e8685892fc
Perkins <803370@suhsd-staff.seq.org>
2021-10-25 19:45:01 +0100

    finished 8.5 and 8.6

--- Cprojects/exercises/8fiveAP.c
+++ Cprojects/exercises/8fiveAP.c
@@ -5,27 +5,55 @@
 #include <ctype.h>
 
 void charHandling(int c);
+void upperLower(char s[100]);
 
 int main()
 {
-    printf("im thinking about it right now");
+    char c = 'a';
+    int cInt = c;
+    charHandling(cInt); 
+
+    char s[100];
+    printf("\n8.6:\n");
+    printf("Enter a string less that or equal to 100 characters:\n");
+	scanf("%[^\n]", s);
+    upperLower(s);
+
     return 0;
 }
 
 void charHandling(int c)
 {
-    printf("%d", isblank(c));
-    printf("%d", isdigit(c));
-    printf("%d", isalpha(c));
-    printf("%d", isalnum(c));
-    printf("%d", isxdigit(c));
-    printf("%d", islower(c));
-    printf("%d", isupper(c));
-    printf("%c", tolower(c));
-    printf("%c", toupper(c));
-    printf("%d", isspace(c));
-    printf("%d", iscntrl(c));
-    printf("%d", ispunct(c));
-    printf("%d", isprint(c));
-    printf("%d", isgraph(c));
+    printf("\n8.5:\n");
+    printf("%d\n", isblank(c));
+    printf("%d\n", isdigit(c));
+    printf("%d\n", isalpha(c));
+    printf("%d\n", isalnum(c));
+    printf("%d\n", isxdigit(c));
+    printf("%d\n", islower(c));
+    printf("%d\n", isupper(c));
+    printf("%c\n", tolower(c));
+    printf("%c\n", toupper(c));
+    printf("%d\n", isspace(c));
+    printf("%d\n", iscntrl(c));
+    printf("%d\n", ispunct(c));
+    printf("%d\n", isprint(c));
+    printf("%d\n", isgraph(c));
+}
+
+void upperLower(char s[100])
+{
+    char* sTemp = s;
+    while (*s != '\0')
+    {
+        printf("%c", toupper(*s));
+        s++;
+    }
+    s = sTemp;
+    printf("\n");
+    while (*s != '\0')
+    {
+        printf("%c", tolower(*s));
+        s++;
+    }
 }
