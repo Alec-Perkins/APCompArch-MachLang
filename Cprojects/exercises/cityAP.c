beb4050db15d9e84f63270e54957f86124ef85f8
Perkins <803370@suhsd-staff.seq.org>
2021-10-29 19:42:49 +0100

    Created cityAP.c

---
+++ Cprojects/exercises/cityAP.c
@@ -0,0 +1,15 @@
+// Exercise 8.21
+// A. Perkins
+
+#include <stdio.h>
+#include <ctype.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    
+    return 0;
+}
