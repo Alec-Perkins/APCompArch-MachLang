6c7531f240d95be151c4c705e77b03f2f27cacc2
Alec R. Perkins <alecperkinsw@gmail.com>
2021-11-10 03:55:35 -0800

    finished genAP.cs

--- Cprojects/projects/gen1AP.c
+++ Cprojects/projects/gen1AP.c
@@ -1,3 +1,6 @@
+// The code seems to work for replacing the first few words such as: "daughter, husband, wife, girl, boy",
+// but the code stops being able to fit the string copy into a temp string the more I edit the line char array
+
 // Gender Neutrality Project
 // A. Perkins
 
@@ -26,15 +29,18 @@ void girl(char line[LENGTH]);
 
 int main(void)
 {
+	// Opens file
 	FILE *fptr;
 	char line[LENGTH];
 	fptr = fopen( FNAME, "r");
 	fgets(line, sizeof(line), fptr);
 
+	// Runs the function to call each word to be replaced
 	replace(line);
 	printf("%s\n",line);
 }
 
+// Calls each function for each word that must be replaced
 void replace(char line[LENGTH])
 {
 	son(line);
@@ -48,23 +54,34 @@ void replace(char line[LENGTH])
 	girl(line);
 }
 
+// The comments on this function are true for all following functions: <------------ IMPORTANT
+// Replaces just one word in the entire file
 void daughter(char line[LENGTH])
 {
+	// Iterates through the file dividing it up by spaces
 	for (int index=0; index<LENGTH; index++)
 	{
 		if (line[index] == ' ')
 		{
+			// Creates a test, replacement word, and word array to put the string slice into
 			char test[] = "daughter";
 			char replace[] = " child";
 			int len = strlen(test);
 			char word[9];
+			// Slices the string to be tested from the file
 			strncpy(word, line + index + 1, len);
+			// Tests to see if the string matches the word being looked for
 			if (strcmp(word, test) == 0)
 			{
+				// Creates a temp for the new full file
 				char temp[strlen(line)+len];
+				// Copies the first part of the file before the replacement word onto the temp
 				strncpy(temp, line, index+1);
+				// Copies the new word onto the temp
 				strncpy(temp + index, replace, strlen(replace)+1);
+				// Copies the remaining file onto the temp
 				strncpy(temp + index + strlen(replace), line + index + len + 1, strlen(line) - index - strlen(replace) + 1);
+				// Sets the old file equal to the new temp
 				strncpy(line, temp, strlen(temp));
 			}
 		}
--- Cprojects/projects/gen2AP.c
+++ Cprojects/projects/gen2AP.c
@@ -1,3 +1,7 @@
+// Works almost completely, however there needs to be more replacement words for every gendered word,
+// and this could go on infinitely. Also the possessive her is different from the noun her so grammar
+// errors occur.
+
 // Gender Neutrality Project
 // A. Perkins
 
@@ -15,29 +19,63 @@ void replace(char line[LENGTH]);
 
 int main(void)
 {
+	// Open file
 	FILE *fptr;
 	char line[LENGTH];
 	fptr = fopen( FNAME, "r");
 	fgets(line, sizeof(line), fptr);
 
+	// Replace the words in the file
 	replace(line);
 
 	return 0;
 }
 
+// Takes the file and replaces the specified words
 void replace(char line[LENGTH])
 {
+	// Creates a token for each word split by spaces and tests them to be replaced
 	char *t;
 	t = strtok(line, " ");
 	while (t != NULL)
 	{
-		printf("%s", t);
 		if (strcmp(t, "girl") == 0 || strcmp(t, "boy") == 0)
 		{
 			t = "person";
 		}
-		printf(" %s ", t);
+
+		else if (strcmp(t, "girls") == 0 || strcmp(t, "boys") == 0)
+		{
+			t = "persons";
+		}
+
+		else if (strcmp(t, "husband") == 0 || strcmp(t, "wife") == 0)
+		{
+			t = "spouse";
+		}
+
+		else if (strcmp(t, "daughter") == 0 || strcmp(t, "son") == 0)
+		{
+			t = "child";
+		}
+
+		else if (strcmp(t, "daughters") == 0 || strcmp(t, "sons") == 0)
+		{
+			t = "children";
+		}
+
+		else if (strcmp(t, "him") == 0 || strcmp(t, "her") == 0)
+		{
+			t = "them";
+		}
+
+		else if (strcmp(t, "he") == 0 || strcmp(t, "she") == 0)
+		{
+			t = "they";
+		}
+
+		printf("%s ", t);
+		// Allows for the program to carry on from where it left off
 		t = strtok(NULL, " ");
 	}
-	// printf("%s", line);
 }
