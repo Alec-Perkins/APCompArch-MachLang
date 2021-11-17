7396086c4b7d82f3cf32930a69839a4158b1e3af
Alec Perkins <alecrperkins@gmail.com>
2021-11-17 21:33:22 +0000

    sync

--- Cprojects/projects/5-3StructLabAPEM.c
+++ Cprojects/projects/5-3StructLabAPEM.c
@@ -6,7 +6,15 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+struct pet {
+	char name[10];
+	char species[10];
+	char color[10];
+	int age;
+	double weight;
+};
+
 int main()
 {
-    puts("let her go let her go god bless her.")
+
 }
