a54576f3dfb8824fdf4e1728f5b338e518c714d6
Alec Perkins <alecrperkins@gmail.com>
2021-11-17 21:01:48 +0000

    Created group project 5-3 Struct Lab

---
+++ Cprojects/projects/5-3StructLabAPEM.c
@@ -0,0 +1,12 @@
+// 5-3 Struct Lab Project
+// A. Perkins, E. Matsuda
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main()
+{
+    puts("let her go let her go god bless her.")
+}
